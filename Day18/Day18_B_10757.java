197fcefa7dda1cbf02d73b97c1b9483db688cf80
M1nsong-Kim <lgms.3222@gmail.com>
2022-09-14 21:45:54 +0900

    Algorithm-Day18

---
+++ Day18/Day18_B_10757.java
@@ -0,0 +1,23 @@
+/*    */ package codingtestAndAlgorithm;
+/*    */ 
+/*    */ import java.io.PrintStream;
+/*    */ import java.math.BigInteger;
+/*    */ import java.util.Scanner;
+/*    */ 
+/*    */ public class Day18_B_10757
+/*    */ {
+/*    */   public static void main(String[] args)
+/*    */   {
+/*  9 */     Scanner sc = new Scanner(System.in);
+/*    */ 
+/* 11 */     BigInteger a = new BigInteger(sc.next());
+/* 12 */     BigInteger b = new BigInteger(sc.next());
+/*    */ 
+/* 14 */     System.out.println(a.add(b));
+/*    */   }
+/*    */ }
+
+/* Location:           D:\코딩\eclipse workspace\first\bin\
+ * Qualified Name:     codingtestAndAlgorithm.Day18_B_10757
+ * JD-Core Version:    0.6.2
+ */
