621eb60acaf5edfb4ccd6b7c46f3b00cbc98dd7d
M1nsong-Kim <110522011+M1nsong-Kim@users.noreply.github.com>
2022-09-17 20:14:38 +0900

    Update Day21_B_11653_2.java

--- Day21/Day21_B_11653_2.java
+++ Day21/Day21_B_11653_2.java
@@ -1,30 +1,23 @@
-/*    */ package codingtestAndAlgorithm;
-/*    */ 
-/*    */ import java.io.BufferedReader;
-/*    */ import java.io.IOException;
-/*    */ import java.io.InputStreamReader;
-/*    */ import java.io.PrintStream;
-/*    */ 
-/*    */ public class Day21_B_11653_2
-/*    */ {
-/*    */   public static void main(String[] args)
-/*    */     throws IOException
-/*    */   {
-/* 10 */     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-/*    */ 
-/* 12 */     int n = Integer.parseInt(br.readLine());
-/*    */ 
-/* 14 */     for (int i = 2; i <= n; i++)
-/*    */     {
-/* 16 */       while (n % i == 0) {
-/* 17 */         n /= i;
-/* 18 */         System.out.println(i);
-/*    */       }
-/*    */     }
-/*    */   }
-/*    */ }
+package codingtestAndAlgorithm;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
-/* Location:           D:\코딩\eclipse workspace\first\bin\
- * Qualified Name:     codingtestAndAlgorithm.Day21_B_11653_2
- * JD-Core Version:    0.6.2
- */
+public class Day21_B_11653_2{
+  
+  public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+         int n = Integer.parseInt(br.readLine());
+
+         for (int i = 2; i <= n; i++) {
+            while (true) {
+              if(n % i == 0){
+                n /= i;
+                System.out.println(i);
+              }else break;
+         }
+  }
+}
